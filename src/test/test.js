4e918e48527d61b1950ed7e70fd19ef14bdc8fa8
Gromak Maxim <maximgromak@gmail.com>
2021-08-28 17:12:33 +1000

    upd: tests

--- src/test/test.js
+++ src/test/test.js
@@ -61,6 +61,23 @@ test('should return sorted arr (3props)', () => {
   expect(actualOutput).toStrictEqual(expectedOutput);
 });
 
+test('should return sorted arr (0prop)', () => {
+  const inputObj = {
+    name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+  };
+  const inputProps = [];
+  const expectedOutput = [
+    { key: 'attack', value: 80 },
+    { key: 'defence', value: 40 },
+    { key: 'health', value: 10 },
+    { key: 'level', value: 2 },
+    { key: 'name', value: 'мечник' },
+  ];
+
+  const actualOutput = orderByProps(inputObj, inputProps);
+
+  expect(actualOutput).toStrictEqual(expectedOutput);
+});
 /*
 incorrect data
 */
