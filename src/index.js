578258fdc764620d760e412a201d35b520d10b6e
Gromak Maxim <maximgromak@gmail.com>
2021-08-28 17:10:17 +1000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+export default function orderByProps(targetObj, propsArr) {
+  const foundProps = [];
+  const otherProps = [];
+  for (const item in targetObj) {
+    const tempObj = { key: item, value: targetObj[item] };
+
+    if (propsArr.find((i) => item === i)) foundProps.push(tempObj);
+    else otherProps.push(tempObj);
+  }
+  return foundProps.concat(
+    otherProps.sort((a, b) => (a.key > b.key ? 1 : -1)),
+  );
+}
---
+++ src/test/test.js
@@ -0,0 +1,66 @@
+import orderByProps from '../index.js';
+
+/*
+correct data
+*/
+
+test('should return sorted arr (1prop)', () => {
+  const inputObj = {
+    name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+  };
+  const inputProps = ['defence'];
+  const expectedOutput = [
+    { key: 'defence', value: 40 },
+
+    { key: 'attack', value: 80 },
+    { key: 'health', value: 10 },
+    { key: 'level', value: 2 },
+    { key: 'name', value: 'мечник' },
+  ];
+
+  const actualOutput = orderByProps(inputObj, inputProps);
+
+  expect(actualOutput).toStrictEqual(expectedOutput);
+});
+
+test('should return sorted arr (2props)', () => {
+  const inputObj = {
+    name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+  };
+  const inputProps = ['name', 'level'];
+  const expectedOutput = [
+    { key: 'name', value: 'мечник' },
+    { key: 'level', value: 2 },
+
+    { key: 'attack', value: 80 },
+    { key: 'defence', value: 40 },
+    { key: 'health', value: 10 },
+  ];
+
+  const actualOutput = orderByProps(inputObj, inputProps);
+
+  expect(actualOutput).toStrictEqual(expectedOutput);
+});
+
+test('should return sorted arr (3props)', () => {
+  const inputObj = {
+    name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+  };
+  const inputProps = ['name', 'level', 'defence'];
+  const expectedOutput = [
+    { key: 'name', value: 'мечник' },
+    { key: 'level', value: 2 },
+    { key: 'defence', value: 40 },
+
+    { key: 'attack', value: 80 },
+    { key: 'health', value: 10 },
+  ];
+
+  const actualOutput = orderByProps(inputObj, inputProps);
+
+  expect(actualOutput).toStrictEqual(expectedOutput);
+});
+
+/*
+incorrect data
+*/
